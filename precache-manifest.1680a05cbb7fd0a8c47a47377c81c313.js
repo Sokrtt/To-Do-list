10290706387a9fa22ba0fc0797fbc97b3d821967
NOrig <sorktt@gmail.com>
2021-02-10 22:39:05 +0600

    Updates

---
+++ precache-manifest.1680a05cbb7fd0a8c47a47377c81c313.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b105e4749f4466fe9fb55d81351058",
+    "url": "/To-Do-list/index.html"
+  },
+  {
+    "revision": "bbc7fb419d815849aca7",
+    "url": "/To-Do-list/static/css/main.00b94472.chunk.css"
+  },
+  {
+    "revision": "724a384ba518726a60da",
+    "url": "/To-Do-list/static/js/2.c32c6ded.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/To-Do-list/static/js/2.c32c6ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc7fb419d815849aca7",
+    "url": "/To-Do-list/static/js/main.95b52630.chunk.js"
+  },
+  {
+    "revision": "e3e1ae266d51d3445499",
+    "url": "/To-Do-list/static/js/runtime-main.be2c0ff8.js"
+  },
+  {
+    "revision": "936f88857e13715003232463b5430273",
+    "url": "/To-Do-list/static/media/edit_icon.936f8885.png"
+  },
+  {
+    "revision": "67166eb15b587a61360b0b6a0828bf7a",
+    "url": "/To-Do-list/static/media/nav_before.67166eb1.png"
+  },
+  {
+    "revision": "1fa8273811e55a27d9842ccfd569043a",
+    "url": "/To-Do-list/static/media/nav_next.1fa82738.png"
+  },
+  {
+    "revision": "bf13633ae6beddfc4662394b1ef91657",
+    "url": "/To-Do-list/static/media/save_icon.bf13633a.png"
+  }
+]);
